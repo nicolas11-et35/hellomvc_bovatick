e1d5abb8d48419adb4c55474a8499fd4dc2b1b7f
Nicolas Bovatick <fariasnico16@gmail.com>
2020-09-16 17:13:16 -0300

    mvc first

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+// routes/index.js
+const express = require('express');
+const controller = require('../controllers/index');
+const router = express.Router();
+
+router.get('/', controller.home);
+
+router.get('/search', controller.search)
+
+module.exports = router;
